77546fefa0684b16f46287bdd18a3e1a26d2b738
marcial <marcial@txtcircuit.com>
2017-09-20 16:55:21 +0800

    CRUD, parse, search, user auth

--- project/app/auth/views.py
+++ project/app/auth/views.py
@@ -16,7 +16,6 @@ def register():
                             last_name=form.last_name.data,
                             password=form.password.data)
 
-        # add employee to the database
         db.session.add(employee)
         db.session.commit()
         flash('You have successfully registered! You may now login.')
@@ -24,7 +23,7 @@ def register():
         # redirect to the login page
         return redirect(url_for('auth.login'))
 
-    # load registration template
+
     return render_template('auth/register.html', form=form, title='Register')
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -33,25 +32,23 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
 
-        # check whether employee exists in the database and whether
-        # the password entered matches the password in the database
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(
                 form.password.data):
-            # log employee in
+
             login_user(user)
 
-            # redirect to the dashboard page after login
+
             if user.is_admin:
                 return redirect(url_for('home.admin_dashboard'))
             else:
                 return redirect(url_for('home.dashboard'))
 
-        # when login details are incorrect
+
         else:
             flash('Invalid email or password.')
 
-    # load login template
+
     return render_template('auth/login.html', form=form, title='Login')
 
 @auth.route('/logout')
@@ -60,5 +57,5 @@ def logout():
     logout_user()
     flash('You have successfully been logged out.')
 
-    # redirect to the login page
+
     return redirect(url_for('auth.login'))
